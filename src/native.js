10957d4ec0a7a587cb114cc35e20fd398fcfcbe0
John Foley <jfoley@cs.umass.edu>
2012-10-03 21:39:40 -0400

    Mock up a "native.js" file
    This is where Javascript versions of core functions will need to be
    implemented. Also, anything reasonable that isn't working from the Java
    Class libraries will need to go here.

---
+++ src/native.js
@@ -0,0 +1,19 @@
+//--- the two methods in here are junk, just to mock up what I think will need to happen "Object.equals" and "String.length"
+
+
+// map of classes first
+var native = {
+  "Ljava/lang/Object" : {
+    "equals": function(vm) {
+      // todo, this is some kind of pointer comparison?
+      vm.setResult(vm.getRegister(0) == vm.getRegister(1))
+    }
+  },
+  "Ljava/lang/String" : {
+    "length": function(vm) {
+      vm.setResult(vm.getRegister(0).strData.length)
+    }
+  }
+}
+
+
