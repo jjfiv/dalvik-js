67ed776197cf8de4eb63db6268497f82901ab64c
Emma Tosch <etosch@gmail.com>
2012-10-15 00:20:32 -0400

    added facility for tracking icodes

--- src/terminal.js
+++ src/terminal.js
@@ -7,8 +7,25 @@
  * but for now it should just offer simple printing.
  *
  */
- 
+
 var DEBUG = true;
+var ICODES = { 
+    codes : {},
+    keys : [],
+    putICODE : function(_key){         
+        if (!ICODES.codes[_key]){
+            ICODES.codes[_key]=_key;
+            ICODES.keys.push(_key);
+        }
+    },        
+    listICODES : function() {
+        var _i, _s="";
+        for (_i = 0 ; _i < ICODES.keys.length ; _i++){
+            _s+=ICODES.keys[_i]+",";
+        }
+        return _s;
+    }
+}
 
 var HTMLTerminal = function() {
   this._output = document.getElementById('output');
@@ -25,6 +42,10 @@ HTMLTerminal.prototype.println = function(str) {
 HTMLTerminal.prototype.appendDebug = function(data, docObj){
     docObj.innerText+=data;
 };
+
+HTMLTerminal.prototype.listICODES = function(docObj){
+    docObj.innerText+=ICODES.listICODES();
+}
 
 //--- global
 var terminal = new HTMLTerminal();
