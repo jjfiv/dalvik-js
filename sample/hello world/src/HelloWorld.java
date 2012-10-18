0a124be882ad8d325ba0ff14ee6e15098e6af0b7
vjd215 <diluoffo@cs.umass.edu>
2012-10-18 11:48:37 -0400

    Documents and Samples

---
+++ sample/hello world/src/HelloWorld.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * @author vjd215
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello, World");
+
+	}
+
+}
