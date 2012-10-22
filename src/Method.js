31c6fc7ed307adfd0ffe9323545a6867d4f9ba63
Emma Tosch <etosch@gmail.com>
2012-10-21 23:35:53 -0400

    revert

--- src/Method.js
+++ src/Method.js
@@ -1,21 +1,46 @@
 // This file contains a definition of a method
+//
+// Dependencies: MethodSignature.js
+//
 
 var Method = function (_name, _definingClass, _paramTypes, _returnType) {
 
   // the name of this method, used for dispatch & debugging
-  this.name = _name;
+  this.signature = new MethodSignature(_name, _returnType, _paramTypes);
 
   // the name of the class that defines this method
   this.definingClass = _definingClass;
 
-  this.paramTypes = _paramTypes;
-  this.returnType = _returnType;
- 
   // the number of registers allocated by this method
   this.numRegisters = 0;
 
+  // permissions of this method
+  this.accessFlags = 0;
+
   // the interpreter-specific code
   this.icode = [];
+};
+
+Method.prototype.getName = function() {
+  return this.signature.getName();
+};
+
+Method.prototype.getReturnType = function() {
+  return this.signature.getReturnType();
+};
+
+Method.prototype.getParameterAmount = function() {
+  return this.signature.getReturnType();
+};
+
+
+//
+// toStr method to build a string of the form
+// Class.methodSignature
+// Class.name(TA,TB)RT
+//
+Method.prototype.toStr = function() {
+  return this.definingClass.toStr() + "." + this.signature.toStr();
 };
 
 Method.prototype.numParameters = function() {
