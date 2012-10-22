90022e92273acc5fc70d59f033383a8e6706fc39
guptha- <guptha@cs.umass.edu>
2012-10-22 13:45:46 -0400

    Added a function to tryCatch sample to demonstrate inheritance of catch, but not try

--- sample/tryCatch/tryCatch.java
+++ sample/tryCatch/tryCatch.java
@@ -1,15 +1,36 @@
+class customException extends Exception {
+  public customException () {}
 
+  public customException (String message) {
+    super ("In custom exception\n");
+  }
+}
 public class tryCatch {
   public static void main(String[] args) {
     try {
-      int k;
+      int k, j;
       k = test (1);
+      j = testThrow (1);
+      throw new customException("Calling custom exception\n");
     }
     catch (ArithmeticException c) {
       System.out.println ("No way!\n");
     }
+    catch (customException c) {
+      System.out.println ("Caught my own exception \n");
+    }
   }
-  static int test (int param) {
+  static int test (int param) throws ArithmeticException {
     return param / 0;
+  }
+  static int testThrow (int param) throws customException{
+    try {
+      if (param == 1)
+      {
+        throw new customException("Calling custom exception\n");
+      }
+    }
+    finally {}
+    return 1;
   }
 }
