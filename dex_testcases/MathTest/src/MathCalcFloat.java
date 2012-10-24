940a463b5b5cfcca7fe3dfc2103ff96ca82858a0
vjd215 <diluoffo@cs.umass.edu>
2012-10-24 16:01:16 -0400

    Add Math tests for int, long, double, float and removed ( math & mathbasic)

---
+++ dex_testcases/MathTest/src/MathCalcFloat.java
@@ -0,0 +1,37 @@
+
+public class MathCalcFloat {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		//Tests for Float operations
+		float x= 2.4f;
+		float y = 4.4f;
+		float z = 0;
+		//addition
+		z = x +y;
+		System.out.println("Sum of entered float = ");
+		System.out.println(z);
+		
+		//subtraction
+		float s = 0;
+		s = y -x;
+		System.out.println("Subtraction of entered float = ");
+		System.out.println(s);
+		
+		//multiple
+		float m = 0;
+		m = x * y;
+		System.out.println("Multiple float = ");
+		System.out.println(m);
+		
+		//divide
+		float d = 0;
+		d = y / x;
+		System.out.println("Divide float = ");
+		System.out.println(d);
+
+	}
+
+}
