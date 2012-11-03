5e5b60187df6d697c32ed9d269960fbcf47dd49a
guptha- <guptha@cs.umass.edu>
2012-11-03 12:45:17 -0400

    Added again mysteriously disappearing test cases bird and recurse

--- vmtest/tests.js
+++ vmtest/tests.js
@@ -4,6 +4,7 @@
 // doTest(fileName, mainClassName, expectedOutput
 
 doTest("factorial.dex", "Lfactorial;", "24\n");
+doTest("recurse.dex", "Lrecurse;", "24\n");
 doTest("seqSwitch.dex", "LSeqSwitch;", "Orange\n");
 // complicated output:
 doTest("testSeveralMethods.dex", "LtestSeveralMethods;",
@@ -15,7 +16,7 @@ doTest("testSeveralMethods.dex", "LtestSeveralMethods;",
 "This is an overloaded fruitless instance method with one arg.\n"+
 "This is an overloaded fruitful static method with one arg.\n"+
 "This is an overloaded fruitful instance method with one arg.\n"+
-"0\n");
+"true\n");
 
 // so it turns out we need to HTML escape our expected output: &gt; &lt; &amp;
 doTest("bitWise.dex", "LbitWiseTests;", 
@@ -43,6 +44,7 @@ doTest("PrimitiveCast.dex",
        "true\ntrue\ntrue\n"
       );
 
+doTest("Bird.dex", "LBird;", "Rexy\ntrue\nfalse\nBlack\ntrue\ntrue\nHumpty\nfalse\nfalse\n");
 
 doTest("ArrayTest.dex", "LArrayTest;", "3,4,5,6\n");
 
